cfef31e9ff4d4ac489ab9fd14460e3627fbfc8b4
Sergio Castillo <scastillonu@uniminuto.edu.co>
2021-08-25 00:33:45 -0500

    Update Tecnologies.js

--- allianceclient/components/Tecnologies/Tecnologies.js
+++ allianceclient/components/Tecnologies/Tecnologies.js
@@ -7,48 +7,48 @@ import SwiperCore, { Autoplay } from "swiper/core";
 SwiperCore.use([Autoplay]);
 
 const Tecnologies = ({ items }) => {
-  const { API_URL } = process.env;
+  const urlcompleta = "https://www.idevcol.com";
 
   return (
     <Fragment>
-      <div className='container carousel-tecnologies'>
-      <Swiper
-      
-      autoplay={{
-        "delay":8000,
-        "speed":2000
-      
-      }}
-        loop={true}
-        slidesPerView={5}
-        breakpoints={{
-          640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-          },
-        }}
-        spaceBetween={20}
-        centeredSlides={true}
-        
-        
-        
-        className="mySwiper"
-      >
+      <div className="container carousel-tecnologies">
+        <Swiper
+          autoplay={{
+            delay: 8000,
+            speed: 2000,
+          }}
+          loop={true}
+          slidesPerView={5}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          spaceBetween={20}
+          centeredSlides={true}
+          className="mySwiper"
+        >
           {items
             ? items.map((item, index) => (
-
-        <SwiperSlide className='carousel-tecnologies__item'><Image src={API_URL + item.Logo[0].url}  width={70} height={50}></Image></SwiperSlide>
-        ))
-        : "Cargando..."}
-      </Swiper>
+                <SwiperSlide className="carousel-tecnologies__item">
+                  <Image
+                    src={urlcompleta + item.Logo[0].url}
+                    width={70}
+                    height={50}
+                  ></Image>
+                </SwiperSlide>
+              ))
+            : "Cargando..."}
+        </Swiper>
       </div>
     </Fragment>
   );
