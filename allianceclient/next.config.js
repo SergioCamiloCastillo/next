4b1a84c3dfb5c229cd20fc6f572639166a3635e6
Sergio Castillo <scastillonu@uniminuto.edu.co>
2021-08-24 23:33:37 -0500

    Update next.config.js

--- allianceclient/next.config.js
+++ allianceclient/next.config.js
@@ -10,7 +10,7 @@ module.exports = {
   images: {
     deviceSizes: [640, 768, 1024, 1280, 1600],
     imageSizes: [16, 32, 48, 64, 96],
-    domains: ['localhost'],
+    domains: ['idevcol.com'],
     path: "/_next/image",
     loader: "default",
   },
