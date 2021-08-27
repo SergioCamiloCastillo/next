877295beab3b402c6e7b72e0619caa63c5c97348
Sergio Castillo <scastillonu@uniminuto.edu.co>
2021-08-27 08:45:32 -0500

    Update _app.js

--- allianceclient/pages/_app.js
+++ allianceclient/pages/_app.js
@@ -39,20 +39,7 @@ export default function MyApp(props) {
         />
         <meta property="og:url" content="https://snipcart.com/" />
         <meta property="og:type" content="website" />
-    <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=206007953
-      />
-      <Script strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 206007953);
-        `}
-      </Script>
-    <script
+   <script
     async
     src="https://www.googletagmanager.com/gtag/js?id=%your code here%" >
 </script>
